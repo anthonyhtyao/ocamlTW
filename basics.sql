f91e3463dc0475bbe6b409648e325ee9ac77d131
cyber-meow <sjungle305@gmail.com>
2017-02-12 02:30:07 +0800

    psql -U postgres -d ocamltw -f basics.sql

--- basics.sql
+++ basics.sql
@@ -1,6 +1,6 @@
-
 UPDATE article
-SET content = 
+
+SET content =
 
 '<h2>變數宣告 (Declaration of variables)</h2>
 
@@ -183,7 +183,7 @@ Exception: Failure "int_of_string".
 </code></pre>
 
 <p>
-呼叫一個 module 的函數使用的語法是 <code>&lt;Module&gt;.&lt;function&gt;</code>，下面舉幾個 String 裡面的例子：
+呼叫一個 module 的函數使用的語法是 <code>&lt;Module&gt;.&lt;function&gt;</code>，下面舉幾個 <code>String</code> 裡面的例子：
 
 <pre><code># String.length "This is a string";; (*字串長度*)
 - : int = 17
@@ -268,6 +268,160 @@ Error: This expression has type (char * string) * int
 
 <p>
 在看過這些基本的 type 後，要記住一件事情，在 OCaml 中，無論兩種資料型別多麼接近，像是 int 和 float 又或者 char 和 string，只要未經過撰寫程式的人明確地進行型別轉換的話，兩個不同型別的值就是不相容的 (no compatible)，也就是說，隱式型別轉換 (implicit type conversion) 並不存在，只要在寫 OCaml，那遇到 type error 就是家常便飯，這在下面運算子的段落你將會有更深的體會，平常寫 JavaScript 習慣的人要頭痛了呢 (笑。
-</p>'
+</p>
 
-WHERE id = 7;
+<h2>運算子 (Operations)</h2>
+
+在這一章即將提到的各種 operation 其實都是定義在 OCaml 的 <a href = ''https://caml.inria.fr/pub/docs/manual-ocaml/libref/Pervasives.html''> Pervasives </a> 模組裡面，此模組在每次編譯時會自動打開，其中包含了各式各樣最為基本的函數，例如前面 Hello World! 章節提到的簡易輸入輸出也是在這裡定義，因此如果想要有更深入的了解不如自己前往模組的網頁。
+
+<h3>整數運算子 (Integer arithmetic)</h3>
+
+<p>
+整數運算子主要有常見的加減乘除以及餘數運算和絕對值，如同大多數的程式語言，除計算的是歐基里德除法的商數，另外 OCaml 中並沒有是先定義好的整數冪次函數。
+</p>
+
+<pre><code># (1 + 5 * 2)/3;;
+- : int = 3
+# 1 / 0;;
+Exception: Division_by_zero.
+# 2017 mod 7;;
+- : int = 1
+# let x = abs (-8);;
+val x : int = 8
+</code></pre>
+
+<h3>浮點數運算子 (Floating-point arithmetic)</h3>
+
+<p>
+來試試看下面的程式碼
+
+<pre><code># <u>0.87</u> +　8.94；；
+Error: This expression has type float but an expression was expected of type int
+</code></pre>
+
+出現了，是野生的 type error，不過我們只不過是要將兩個小數相加，怎麼就出問題了呢？事實上就像前面提到的，OCaml 對 type 的管理非常嚴格，這份嚴謹正是構成了 OCaml 強大的地方，這點我們在 Why OCaml? 的章節裡已經再三強調過了。因此若要對浮點數做運算，運算子後面都要加上一個 . 。
+
+<pre><code># 0.87 +. 8.94;;
+- : float = 9.78
+# 3.14 *. 2.71 /. 1.41;;
+- : float = 6.03503546099290755
+# 1. /. 0.;;
+- : float = infinity
+</code></pre>
+
+同理推得在 OCaml 是沒辦法像大多數語言一樣直接做整數與小數間的運算的，必須先利用 <code>int_of_float</code> 和 <code>float_of_int</code> 進行 type conversion。一些常用到的數學函數也可以直接呼叫 (詳見 <a href = ''https://caml.inria.fr/pub/docs/manual-ocaml/libref/Pervasives.html''> Pervasives </a>  網頁)。
+
+
+<pre><code># int_of_float (-3.7);;
+- : int = -3;;
+# float_of_int 87 +. 0.87;;
+- : float = 87.87;;
+# 2. ** 3.;;
+- : float = 8.;;
+# exp 1.;;
+- : float = 2.71828182845904509
+</code></pre>
+</p>
+
+<h3>比較 (Comparisons) 和布林運算子 (Boolean operations)</h3>
+
+<p>
+比較中最簡單的莫過於大小比較，OCaml 中不只能比較整數、浮點數，也能夠對兩個字元、字串等等比大小，甚至是我們自己定義的 type，它也能夠舉一反三的自己在上面推論出一個 total ordering 呢！(後面兩個例子可以在看完自定義 type 的章節再回來品味。)
+</p>
+
+<pre><code># 1 &lt; 3;;
+- : bool = true
+# ''a'' &gt; ''#'';;
+- : bool = true
+# "two" &lt;= "three";;
+- : bool = false
+# (2.8,3,5) &gt;= (1.2,7,8);;
+- : bool = true
+
+# type t = N of char*string;;
+type t = N of char*string
+# N(''s'',"rhombencephalon") &lt; N(''z'',"mitochondrion");;
+- : bool = true
+# type anime = { year:int; name:string };;
+type anime = { year : int; name : string; }
+# {year = 2006; name = "Code Geass"} &lt; {year = 1996, name = "Neon Genesis Evangelion"}
+- : bool = false
+</code></pre>
+
+<p>
+接下來要談的是比較複雜的等於的部分，這個每個語言都有自己的一套玩法，像是 Python 裡面新定義的 class 直接使用 <code>==</code> 相當於問說兩個東西是否為同一個物件，如果希望有更客製化的結果則要自己寫 <code>__eq__</code> 的 method，或者說 JavaScript 必須要寫 <code>===</code> 才會對兩個變數的 type 做比較等等。而在 OCaml 中，最常使用到的等於是 <code>=</code>，只有一個等於，我們稱之為 structural equality，它會由程式自己計算，進而判斷兩個東西在結構上是否相同，也試用於自己定義的型別，不等於則寫作 <code>&lt;&gt;</code>。這種等於和大小比較，面對 <code>ref</code> 或者 mutable 的結構，比較的會是儲存的內容 (詳見後面章節)。
+</p>
+
+<pre><code># 3.7 = 3.7;;
+- : bool = true
+# let a = ''a'' in let b = ''b'' in a &lt;&gt; b;;
+- : bool = true
+# [2;3;5;7] = [2;3;5;7];; (*比較 list*)
+- : bool = true
+# N(''@'', "el psy congroo") = N(''@'', "el psy congroo");; (*繼續使用前面定義的 type*)
+- : bool = true
+# let a = ref 0 in let b = ref 0 in a = b;; (*比較 reference*)
+- : bool = true
+</code></pre>
+
+<p>
+在 OCaml 中 <code>==</code> 和 <code>!=</code> 也是存在的，不過比較的是 physical eqaulity，問的是兩個東西在記憶體中儲存的是否是一樣的內容，為此必須對物件如何在記憶體中儲存有大概的理解，實際上直覺地說，對於 mutable 的東西，我們問的就是這兩個物品是否會同時被修改，而對於 non-mutable 的型別，網站上也寫說它的結果是 implementation-dependent，除非真的很有信心不然平常盡量避免使用。而如果你不小心搞錯把想用的等於從 <code>=</code> 寫成 <code>==</code>，那就不免要得到滿滿的 <code>false</code> 囉。 
+</p>
+
+<pre><code># 247 == 247
+- : bool = true
+# 3.7 == 3.7;;
+- : bool = false
+# let a = 3.7 in a == a;;
+- : bool = true
+# [2;3;5;7] == [2;3;5;7];;
+- : bool = false
+# [] == [];;
+- : bool = true
+# N(''@'', "el psy congroo") == N(''@'', "el psy congroo");;
+- : bool = false
+# let a = ref 0 in let b = ref 0 in a == b;; 
+- : bool = false
+# let a = ref 0 in let b = a in a == b;;
+- : bool = true
+</code></pre>
+
+上面寫得有點長了，但要記住的重點只有一個，在 OCaml 中，平常要知道兩個東西是否相等就使用 <code>=</code> 和 <code>&lt;&gt;</code> 吧。最後講解一下常見的邏輯運算子，包含且 (<code>&&</code>)、或 (<code>||</code>) 和否定 (<code>not</code>)，在 OCaml 中的 and 和 or 是 lazy 的。
+
+<pre><code># true || false;;
+- : bool = true
+# not (5 > 7) && infinity > max_float;;
+- :　bool　= true
+# nan < infinity && 1/0 = 1;;
+- : bool = false
+</code></pre>
+
+<h3>位元運算子 (Bitwise Operations) 及其他</h3>
+
+<p>
+先從一個上面沒有提到但蠻重要的運算子提起吧，若要將兩個字串連在一起，使用的是 <code>^</code> 這個奇怪的符號。
+</p>
+
+<pre><code># "C" ^ "8763";;
+- : string = "C8763"
+</code></pre>
+
+作為本節的結束，我們要來談位元運算子，也就是將整數看作其二進位表示並將其逐位操作的運算子，不過由於這基本上平常根本不會用到，所以其實你大可跳過這一段。位元運算子首先包含 bit by bit 的 and, or, xor 和 not 分別是 <code>land</code>, <code>lor</code>, <code>lxor</code> 和 <code>lnot</code>，舉例來說對應到 Java 的話是 <code>&</code> , <code>|</code> , <code>^</code> 以及 <code>~</code>。另外還有左移以及右移運算子，<code>lsl</code> 向左移右邊補零，<code>lsr</code> 像右移左邊補零 (logical shift)，<code>asr</code> 像右移左邊依照正負號來補 0 或 1 (arithmetic shift)，因此他們分別是對應到 Java 的 <code>&lt;&lt;</code> , <code>&gt;&gt;&gt;</code> 還有 <code>&gt;&gt;</code>。另外移的位數如果是負的或者超過位元限制，會壞掉的請小心。
+
+<pre><code># 14 land 21;;
+- : int = 4
+# lnot 15;;
+- : int = -16
+# 5 lsl 3;;
+- : int = 40
+# (-73) asr 2;;
+- : int = -19
+# (-73) lsr 2;; (*左邊補零*)
+- : int = 2305843009213693933
+# 3 lsl -3;; (*要壞掉了*)
+- : int = -2305843009213693952
+</code></pre>
+
+就這樣，第一節終於結束了 (灑花)，雖然看起來很長不過都是基礎的內容其實沒有什麼東西，下一節講到函數我們才會真正了解到 OCaml 這個語言的特色以及魅力所在 (after all, it is a functional language)，看個點兔休息一下，第二節的長度可能是第一節的兩倍以上喔。'
+
+WHERE id = 7
